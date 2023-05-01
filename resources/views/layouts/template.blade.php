91d4f0880b319cb0b96b13ff7a57ee157467c8f7
Annisa Lathifa <annisalathifa1234@gmail.com>
2023-05-01 22:15:50 +0700

    tes

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,141 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>About Us</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="resources/css/app.css"/>
+    <link rel="stylesheet" href="resources/js/bootstrap.js">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/js/bootstrap.js'])
+
+    <!-- Bootstrap core CSS -->
+
+    <!--
+            <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    -->
+
+    <!-- Custom styles for this template -->
+    <link href="album.css" rel="stylesheet">
+  </head>
+
+  <body style="background-color: #D9F9EE;">
+
+    <main role="main">
+
+      <section class="jumbotron text-center shadow" style="background-color:#F1FAEE ;">
+        <div class="container">
+          <h1 class="jumbotron-heading">ABOUT US</h1>
+          <p class="lead text-muted">SADIS atau Sistem Pengolahan Sampah Medis B3 adalah sebuah situs web yang menyediakan layanan pembuangan Sampah B3 (Bahan Berbahaya dan Beracun), khususnya untuk sampah medis. Website ini bertujuan untuk mengurangi dan mengelola sampah b3, khususnya medis agar tidak berceceran di sembarang tempat.</p>
+      </section>
+
+      <div class="album py-5">
+        <p class="fs-2 p-2 text-center fw-bolder fs-1" style="font-size: large; font-weight: bold;">OUR TEAM</p>
+        <div class="container">
+
+            <!-- Anggota 1 -->
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow rounded-6 border-0 shadow" style="border-radius: 7px; background-color: #FDFEF3; ">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text fs-2" style="font-size: medium;">Niswah Nasyithah</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted fs-4" style="font-size: small;">Front-End Developer</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Anggota 2 -->
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow rounded-6 border-0 shadow" style="border-radius: 7px; background-color: #FDFEF3;">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text fs-2" style="font-size: medium;">Muhammad Akbarul Ihsan</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted fs-4" style="font-size: small;">Back-End Developer</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Anggota 3 -->
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow rounded-6 border-0 shadow" style="border-radius: 7px; background-color: #FDFEF3;">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text fs-2" style="font-size: medium;">Arrijalul Khairi</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted fs-4" style="font-size: small;">Front-End Developer</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <!-- Anggota 4 -->
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow rounded-6 border-0 shadow" style="border-radius: 7px; background-color: #FDFEF3;">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text fs-2" style="font-size: medium;">Annisa Lathifa</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted fs-4" style="font-size: small;">Front-End Developer</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Anggota 5 -->
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow rounded-6 border-0 shadow" style="border-radius: 7px; background-color: #FDFEF3;">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text f-2" style="font-size: medium;">Kelsy Amirah</p>
+                  <div class="d-flex justify-content-between align-items-end">
+                    <small class="text-muted fs-4" style="font-size: small;">Front-End Developer</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            
+
+          </div>
+        </div>
+
+
+    </main>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <!--
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    -->
+
+
+
+  </body>
+</html>
